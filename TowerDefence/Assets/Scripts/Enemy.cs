d8e984d355e995d670df66be873937a8c67f7f2d
sale7268 <hsaleem@csumb.edu>
2020-10-25 21:24:53 -0700

    Level1 half done

---
+++ TowerDefence/Assets/Scripts/Enemy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+  public Waypoints[] navPoints;
+  private Transform target;
+  private Vector3 direction;
+  public float amplify = 1;
+  private int index = 0;
+  private bool move = true;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    //Place our enemy at the start point
+    transform.position = navPoints[index].transform.position;
+    NextWaypoint();
+    
+    //Move towards the next waypoint
+    //Retarget to the following waypoint when we reach our current waypoint
+    //Repeat through all of the waypoints until you reach the end
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (move)
+    {
+      transform.Translate(direction.normalized * Time.deltaTime * amplify);
+
+      if ((transform.position - target.position).magnitude < .1f)
+      {
+        NextWaypoint();
+      }
+    }
+
+  }
+
+  private void NextWaypoint()
+  {
+    if (index < navPoints.Length - 1)
+    {
+      index += 1;
+      target = navPoints[index].transform;
+      direction = target.position - transform.position;
+    }
+    else
+    {
+      move = false;
+    }
+  }
+
+
+}
